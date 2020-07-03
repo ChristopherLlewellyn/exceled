96b7f60a48aa81c9d942d6fc1969698c7b7e7c31
Chris <christerface@gmail.com>
2020-07-03 22:53:42 +0100

    Added ability to download system samples (need to update links)

--- client/src/app/landing/systems/systems.component.ts
+++ client/src/app/landing/systems/systems.component.ts
@@ -8,6 +8,15 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SystemsComponent implements OnInit {
 
+  // TODO: Update download links
+  downloadLinks = {
+    budgetModel: 'https://drive.google.com/uc?export=download&id=1OclzcJ1silObwxhwMac4xPU5G4_VPTUf',
+    managingNonPublicFunds: 'https://drive.google.com/uc?export=download&id=1OclzcJ1silObwxhwMac4xPU5G4_VPTUf',
+    teachersSalaryAssessment: 'https://drive.google.com/uc?export=download&id=1OclzcJ1silObwxhwMac4xPU5G4_VPTUf',
+    staffSalaryCalculator: 'https://drive.google.com/uc?export=download&id=1OclzcJ1silObwxhwMac4xPU5G4_VPTUf',
+    customerDatabaseAndInvoicingSystem: 'https://drive.google.com/uc?export=download&id=1OclzcJ1silObwxhwMac4xPU5G4_VPTUf'
+  };
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
